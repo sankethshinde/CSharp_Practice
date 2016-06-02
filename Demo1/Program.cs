945165a8b231eb28901285c439aff0656ee78c05
unknown <sshinde@Austin.internal.tavisca.com>
2016-06-02 14:57:26 +0530

    Updated with If and Switch

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -77,9 +77,109 @@ namespace Demo1
 
             Console.WriteLine(EvenNumbers[0]);
 
+        
+            // For comments demo
+            //SampleClass 
+
+            Console.WriteLine(" Please enter a number : ");
+
+            int NumberToAdd = int.Parse(Console.ReadLine());
+
+            if (NumberToAdd == 10)
+            {
+                Console.WriteLine("Successful");
+            }
+            else if(NumberToAdd < 100)
+            {
+                Console.WriteLine("Unsuccessful");
+            }
+            // || if first true then doesnt check 2nd. | means check both the conditions irrespectively
+            if (NumberToAdd == 10 | NumberToAdd == 11)
+            {
+                Console.WriteLine(" Number is between 10 or 11");
+            }
+
+
+            switch (NumberToAdd)
+            {
+                case 10:
+                case 20:
+                case 30:
+                    Console.WriteLine("Enter number is {0}", NumberToAdd);
+                    break;
+
+                case 40:
+                case 50:
+                    Console.WriteLine("Enter number is {0}", NumberToAdd);
+                    break;
+                default:
+                    Console.WriteLine("Numbers are not from 10 20 or 30");
+                    break;
+
+            }
+
+            Start:
+            Console.WriteLine(" Select your coffee : 1. Small 2.Medium 3.Large - ");
+
+            int UserChoice = int.Parse(Console.ReadLine());
+
+
+            switch (UserChoice)
+            {
+                case 1:
+                    Console.WriteLine(" 10 $");
+
+                    break;
+
+
+                case 2:
+                    Console.WriteLine(" 20 $ ");
+
+                break;
+
+                case 3:
+                Console.WriteLine(" 30 $ ");
+
+                break;
+
+                default:
+                Console.WriteLine("User enter choice {0} is invalid", NumberToAdd);
+                break;
+
+
+            }
+
+            Console.WriteLine("Do you want to buy more coffee");
+
+            string UserDecision = Console.ReadLine();
+
+            switch (UserDecision)
+            {
+                case "Yes":     
+                case "Y":
+                case "y":
+                    goto Start;
+
+                case "No":
+                case "N":
+                case "n":
+                    break;
+            }
+
+
+
+
+
+
+
             Console.Read();
-
-
         }
     }
+
+   /// <summary>
+   /// This is sample class for demos.
+   /// </summary>
+    public class SampleClass
+    { }
 }
+
